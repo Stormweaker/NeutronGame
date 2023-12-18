43fe764667961af152d9650a884dacc1a72f2950
Stormweaker <luthz@hotmail.fr>
2023-12-18 02:47:38 +0100

    Correction calcul angle et direction

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
 @export var speed:float = 100.0
-@export var angle:float = 45.0
+@export var angle_deg:float = 45.0
 
-var go_up:bool = true
+var angle: float = deg_to_rad(angle_deg)
+var go_down:bool = true
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("change_direction"):
-		go_up = !go_up
+		go_down = !go_down
 
 
 func _physics_process(delta: float) -> void:
-	var go_up_int:int = int(go_up) * 2 - 1
-	var direction:Vector2 = Vector2(cos(angle),go_up_int * sin(angle)).normalized()
+	var go_down_int:int = int(go_down) * 2 - 1
+	var direction:Vector2 = Vector2(cos(angle),go_down_int * sin(angle)).normalized()
+	print(direction)
 	velocity = speed * direction
 	move_and_slide()
 
